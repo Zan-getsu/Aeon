1e377f6ed492b11563deb0ef8b1be03d95bcadae
Zangetsu <121733488+Zan-getsu@users.noreply.github.com>
2024-04-17 00:05:08 +0600

    Add files via upload

--- update.py
+++ update.py
@@ -1,29 +1,31 @@
-from os import path, environ
-from subprocess import run
-from requests import get
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+from os import path as ospath, environ, execl as osexecl
+from subprocess import run as srun
+from requests import get as rget
 from dotenv import load_dotenv
+from sys import executable
 from pymongo import MongoClient
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error, info
 
-if path.exists('log.txt'):
+if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-basicConfig(format='%(message)s', handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
+basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
+                    handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
 
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
     if len(CONFIG_FILE_URL) == 0:
         raise TypeError
     try:
-        res = get(CONFIG_FILE_URL)
+        res = rget(CONFIG_FILE_URL)
         if res.status_code == 200:
             with open('config.env', 'wb+') as f:
                 f.write(res.content)
         else:
-            error(f"Failed to download config.env {res.status_code}")
+            log_error(f"Failed to download config.env {res.status_code}")
     except Exception as e:
-        error(f"CONFIG_FILE_URL: {e}")
+        log_error(f"CONFIG_FILE_URL: {e}")
 except:
     pass
 
@@ -31,7 +33,7 @@ load_dotenv('config.env', override=True)
 
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 if len(BOT_TOKEN) == 0:
-    error("BOT_TOKEN variable is missing! Exiting now")
+    log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
@@ -56,10 +58,10 @@ UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
     UPSTREAM_BRANCH = 'main'
 
-if path.exists('.git'):
-    run(["rm", "-rf", ".git"])
+if ospath.exists('.git'):
+    srun(["rm", "-rf", ".git"])
 
-update = run([f"git init -q \
+update = srun([f"git init -q \
                  && git config --global user.email yesiamshojib@gmail.com \
                  && git config --global user.name 5hojib \
                  && git add . \
@@ -69,6 +71,6 @@ update = run([f"git init -q \
                  && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
 if update.returncode == 0:
-    info('Successfully updated with latest commit from UPSTREAM_REPO')
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
-    error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
